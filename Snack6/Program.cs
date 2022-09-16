0dea8ced204cf5d8e0515783caa819f7ea73479a
RubaCodes <rubacodes@gmail.com>
2022-09-16 15:29:01 +0200

    snack6

---
+++ Snack6/Program.cs
@@ -0,0 +1,15 @@
+﻿//Snack6: array nomi festa gatsby, utente inserisce nome e vede se contenuto nella lista
+string[] invitati = { "Marco", "Luca", "Giovanni" };
+
+Console.WriteLine("Inserisci il tuo nome:");
+string name = Console.ReadLine();
+
+bool checkname = false;
+for (int i = 0; i < invitati.Length; i++) {
+    if (invitati[i].ToUpper() == name.ToUpper()) {
+        checkname = true;
+    }
+}
+
+//checkname == false ? Console.WriteLine("Non sei inviato alla festa") : Console.WriteLine("Sei invitato entra pure");
+Console.WriteLine(checkname == false ? "Non sei invitato": "Sei invitato");
