c24592333418080af6eb7fc89ff6c0d336ef7a0f
RubaCodes <rubacodes@gmail.com>
2022-09-16 15:17:05 +0200

    Implementato snack 5

---
+++ Snack5/Program.cs
@@ -0,0 +1,9 @@
+﻿//Snack5 : Il software chiede allútente di inserire un numero, se pari stampa "pari altrimenti "disparí"
+Console.WriteLine("Inserire numero intero:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0) {
+    Console.WriteLine("Il numero inserito é pari");
+}
+else { Console.WriteLine("Il numero inserito é dispari");
+}
