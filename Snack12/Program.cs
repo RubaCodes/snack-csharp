8835c168f4e4544ca23cadb0e9c83199235b4fd2
RubaCodes <rubacodes@gmail.com>
2022-09-16 16:28:30 +0200

    snack12

---
+++ Snack12/Program.cs
@@ -0,0 +1,11 @@
+﻿
+static bool isEven(int number)
+{
+    if (number % 2 == 0) return true;
+    else return false;
+}
+
+Console.WriteLine("Inserire numero");
+int input = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine( isEven(input) ? $"In numero {input} é  pari" : $"Il numero {input} é dispari" );
