3ead9b1b29cfa04e7bb09fab367f2cafc68c03d6
RubaCodes <rubacodes@gmail.com>
2022-09-16 14:17:09 +0200

    Add project files.

---
+++ Snack1/Program.cs
@@ -0,0 +1,5 @@
+﻿//L'utente inserisce due numeri in successione, il software stampa il maggiore;
+
+//primo input
+Console.WriteLine("Inserire il primo numero:");
+double num1 = Convert.ToDouble(Console.ReadLine());
