8c5bcefdb4818c6305ae0b749bb436d6363792e3
RubaCodes <rubacodes@gmail.com>
2022-09-16 14:33:59 +0200

    implelementato snack2

---
+++ Snack2/Program.cs
@@ -0,0 +1,17 @@
+﻿//L'utente inserisce du parole in successione, il software stampa prima la parola piu corta, poi la piu lunga
+Console.WriteLine("Inserire la prima parola:");
+string firstWord = Console.ReadLine();
+Console.WriteLine("Inserire la seconda parola:");
+string secondWord = Console.ReadLine();
+
+if (firstWord.Length > secondWord.Length)
+{
+    Console.WriteLine($"La parola {firstWord} é piu lunga della parola {secondWord}");
+}
+else if (firstWord.Length < secondWord.Length)
+{
+    Console.WriteLine($"La parola {secondWord} é piu lunga della parola {firstWord}");
+}
+else {
+    Console.WriteLine("Le due parole hannno lunghezza uguale");
+}
