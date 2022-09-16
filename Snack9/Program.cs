c47874e5bd915a0450d4da1b184396ee96f830dc
RubaCodes <rubacodes@gmail.com>
2022-09-16 15:53:48 +0200

    snack9 forse?

---
+++ Snack9/Program.cs
@@ -0,0 +1,9 @@
+﻿//snack9
+double[] array = { 0 };
+while (array[0] < 50) {
+    Console.WriteLine("Inserisci un numero");
+
+    array[0] += Convert.ToDouble(Console.ReadLine());
+}
+
+Console.WriteLine("Hai superato 50!");
