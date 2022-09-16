280614ceea071218a72680074497092882acb424
RubaCodes <rubacodes@gmail.com>
2022-09-16 14:22:30 +0200

    Implementazione snack1

--- Snack1/Program.cs
+++ Snack1/Program.cs
@@ -3,3 +3,16 @@
 //primo input
 Console.WriteLine("Inserire il primo numero:");
 double num1 = Convert.ToDouble(Console.ReadLine());
+//secondo input
+Console.WriteLine("Inserire il secondo numero:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+if (num1 == num2)
+{
+    Console.WriteLine("I due numeri immessi sono uguali");
+}
+else if (num1 > num2)
+{
+    Console.WriteLine($"{num1} é maggiore di {num2}");
+}
+else { Console.WriteLine($"{num2} é maggiore di {num1}"); }
