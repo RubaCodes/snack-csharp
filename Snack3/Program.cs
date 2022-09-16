3e12b7b66de58d103dcbb8682a78fabb9745ebe2
RubaCodes <rubacodes@gmail.com>
2022-09-16 14:45:27 +0200

    implementato snack3

---
+++ Snack3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Snack3: il programma chiede all'utente di inserire 10 volte un numero, il programma stampa la somma
+double sum = default;
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Inserisci un numero:");
+    sum += Convert.ToDouble(Console.ReadLine());
+}
+
+Console.WriteLine("La somma vale: " + sum);
+
+//con ciclo while
+int j = 0;
+sum = 0;
+while (j < 10) {
+    Console.WriteLine("Inserisci un numero:");
+    sum += Convert.ToDouble(Console.ReadLine());
+    j++;
+}
+Console.WriteLine("La somma vale: " + sum);
