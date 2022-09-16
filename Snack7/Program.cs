0faf4568bd3c6567388f6d6148d5964ba1fe6245
RubaCodes <rubacodes@gmail.com>
2022-09-16 15:38:27 +0200

    snack7 iniziale

---
+++ Snack7/Program.cs
@@ -0,0 +1,15 @@
+﻿//snack7: Crea un array vuoto, chiedi 6 volte all'utente di inserire un numero , se disapri inseriscilo nell'array
+
+int[] array = new int[6];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Inserisci un numero");
+    int numero = Convert.ToInt32(Console.ReadLine());
+    if (numero % 2 != 0) {
+        array[i] = numero;
+    }
+}
+for (int i = 0; i < array.Length; i++) {
+    Console.WriteLine(array[i]);
+}
